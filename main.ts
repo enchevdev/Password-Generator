1b7d7a095d64afc3271318212cd2bb400e422279
enchevdev <enchev.dev@gmail.com>
2026-09-04 06:59:07 +0200

    Add password character sets and length

---
+++ main.ts
@@ -0,0 +1,7 @@
+const kleinLetters: string = "azertyuiopqsdfghjklmwxcvbn";
+const hoofdLetters: string = "AZERTYUIOPQSDFGHJKLMWXCVBN";
+const cijfers: string = "0123456789";
+const specialeTekens: string = "&$*`£/+=-;?@#";
+const lengteWachtwoord: number = 12;
+
+console.log(lengteWachtwoord);
